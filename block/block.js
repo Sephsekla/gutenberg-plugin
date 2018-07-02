0c869d20c02aa583eac832ac3f9af0440acf6994
Sephsekla <dev@joebr.io>
2018-07-02 14:22:37 +0100

    Block attributes added

--- block/block.js
+++ block/block.js
@@ -16,7 +16,60 @@
         title: i18n.__( 'Feature' ), // The title of our block.
         icon: 'info', // Dashicon icon for our block
         category: 'common', // The category of the block.
-        attributes: {}, /* Placeholder */
+        attributes: { // Necessary for saving block content.
+            titleOne: {
+              type: 'array', //Title for first feature
+              source: 'children',
+              selector: '.gfblocks-title-1',
+            },
+            textOne: {
+              type: 'array', //Text for first feature
+              source: 'children',
+              selector: '.gfblocks-text-1',
+            },
+            mediaIDOne: {
+              type: 'number', // Media ID for first feature
+            },
+            mediaURLOne: {
+              type: 'string', // Media URL for first feature
+              source: 'attribute',
+              selector: '.gfblocks-feature-image-1 img',
+              attribute: 'src',
+            },
+            hrefOne: {
+              type: 'url', // Link for first feature
+            },
+            titleTwo: {
+              type: 'array',
+              source: 'children',
+              selector: '.gfblocks-title-2',
+            },
+            textTwo: {
+              type: 'array',
+              source: 'children',
+              selector: '.gfblocks-text-2',
+            },
+            mediaIDTwo: {
+              type: 'number',
+            },
+            mediaURLTwo: {
+              type: 'string',
+              source: 'attribute',
+              selector: '.gfblocks-feature-image-2 img',
+              attribute: 'src',
+            },
+            hrefTwo: {
+              type: 'url',
+            },
+            alignment: {
+              type: 'string',
+              default: 'center',
+            },
+            columns: {
+              type: 'select', //Number of columns
+              default: '2'
+            }
+          },
         edit: {}, /* Placeholder */
         save: {} /* Placeholder */
       })
